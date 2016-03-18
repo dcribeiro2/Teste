428398b81037160615ccba427de3a10bc75f2eb9
dcribeiro2 <dcribeiro2@WS-LAB02_126.univel.univel.br>
2016-03-18 18:58:54 -0300

    Primeiro commit

---
+++ src/br/univel/Principal.java
@@ -0,0 +1,13 @@
+package br.univel;
+	public class Principal {
+
+		public static void main(String[] args) {
+			System.out.println("Hello World");
+			System.out.println("Open Bar");
+			 
+		}
+		 
+	}
+
+
+
